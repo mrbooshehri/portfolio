8cb5084751851f372bc2863abb85eebb5d310979
mohammadreza booshehri <mrbooshehri@gmail.com>
2024-12-17 14:04:47 +0330

    update skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -163,8 +163,8 @@ const skills = [
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
+        width="40"
+        height="40"
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
@@ -201,8 +201,8 @@ const skills = [
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
+        width="40"
+        height="40"
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
