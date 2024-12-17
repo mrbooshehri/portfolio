04e737082ae308c9c0d1869fbe4961ff6bd0bca4
mohammadreza booshehri <mrbooshehri@gmail.com>
2024-12-17 10:05:40 +0330

    update contact information

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -41,7 +41,7 @@ export default function Contact() {
                 <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
                 <path d="M3 7l9 6l9 -6" />
               </svg>
-              <span>contact@example.com</span>
+              <span></span>
             </div>
             <div className="flex items-center gap-4 text-gray-300">
               <svg
@@ -59,7 +59,7 @@ export default function Contact() {
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
               </svg>
-              <span>+1 (555) 123-4567</span>
+              <span>+98 (936) 145 6697</span>
             </div>
             <div className="flex items-center gap-4 text-gray-300">
               <svg
@@ -78,7 +78,7 @@ export default function Contact() {
                 <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
                 <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
               </svg>
-              <span>San Francisco, CA</span>
+              <span>Tehran, Tehran</span>
             </div>
           </div>
           <div>
