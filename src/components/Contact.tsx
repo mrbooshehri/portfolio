7565a3aad855fd5ba83dd84d75764153437a8b31
mohammadreza booshehri <mrbooshehri@gmail.com>
2024-12-17 10:29:21 +0330

    add email to contacts

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -41,7 +41,7 @@ export default function Contact() {
                 <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
                 <path d="M3 7l9 6l9 -6" />
               </svg>
-              <span></span>
+              <span>mrbooshehri@gmail.comi]k</span>
             </div>
             <div className="flex items-center gap-4 text-gray-300">
               <svg
